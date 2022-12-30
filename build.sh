3e35835e88b3365a81dba0bc2f89438558cb0f4a
wrongbaud <wrongbaud@gmail.com>
2022-12-30 18:06:59 -0500

    Adding update to build script, performing menuconfig before build

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ sudo cp /usr/xcc/mipsel-unknown-linux-gnu/bin/mipsel-unknown-linux-gnu-objcopy $
 sudo cp toolchain/buildroot-gcc342/libexec/gcc/mipsel-linux-uclibc/3.4.2/cc* $TOOLDIR 
 
 export CROSS_COMPILE=$TOOLDIR
+make menuconfig
 make
